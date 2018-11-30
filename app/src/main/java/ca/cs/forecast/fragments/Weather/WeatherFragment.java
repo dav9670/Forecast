a55197333be5498b86db76f0e3af6bbd2e8446de
davidL <david.lafreniere@live.ca>
2018-11-29 20:56:28 -0500

    WeatherFragment: Show city position on map

--- app/src/main/java/ca/cs/forecast/fragments/Weather/WeatherFragment.java
+++ app/src/main/java/ca/cs/forecast/fragments/Weather/WeatherFragment.java
@@ -1,5 +1,6 @@
 package ca.cs.forecast.fragments.Weather;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,19 +8,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import ca.cs.forecast.R;
+import ca.cs.forecast.data.CityViewModel;
+import ca.cs.forecast.model.City;
 
 public class WeatherFragment extends Fragment implements OnMapReadyCallback {
 
     private SupportMapFragment mMapFragment;
     private GoogleMap mMap;
+    private City mCity;
+
 
     public WeatherFragment() {
-
     }
 
     @Override
@@ -31,6 +38,8 @@ public class WeatherFragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_weather, container, false);
+
+        mCity = ViewModelProviders.of(getActivity()).get(CityViewModel.class).getSelectedItem();
 
         mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
@@ -52,5 +61,10 @@ public class WeatherFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng pos = new LatLng(mCity.getLatitude(), mCity.getLongitude());
+        mMap.addMarker(new MarkerOptions()
+                .position(pos)
+                .title(mCity.getName()));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 5));
     }
 }
